3de82c5d36d3612f2e66ea313ef8aa64c5b273f7
jaipreethundal <jhundal2021>
2020-05-22 04:02:05 -0700

    Finished the lab!

---
+++ src/lab/assignment/beforerefactoring/AmericanoCoffee.java
@@ -0,0 +1,13 @@
+package lab.assignment.beforerefactoring;
+
+public class AmericanoCoffee extends CoffeeMakerTemplate{
+    @Override
+    protected void addIngredients() {
+        System.out.println("Add more hot water");
+    }
+
+    @Override
+    protected void finalTouch() {
+        System.out.println("Add sugar and cream");
+    }
+}
--- src/lab/assignment/beforerefactoring/clientcode/CoffeeClient.java
+++ src/lab/assignment/beforerefactoring/clientcode/CoffeeClient.java
@@ -1,17 +1,24 @@
 package lab.assignment.beforerefactoring.clientcode;
 
-import lab.assignment.beforerefactoring.CoffeeMaker;
+import lab.assignment.beforerefactoring.AmericanoCoffee;
+import lab.assignment.beforerefactoring.CapuccinoCoffee;
+import lab.assignment.beforerefactoring.CoffeeMakerTemplate;
+import lab.assignment.beforerefactoring.MochaCoffee;
 
 public class CoffeeClient {
 
 
     public static void main(String[] args) {
-        CoffeeMaker starbuzz = new CoffeeMaker();
-        starbuzz.prepareMochaCoffee();
+        CoffeeMakerTemplate starbuzz = new MochaCoffee();
+        starbuzz.prepareCoffee();
 
         System.out.println("********************");
+        starbuzz = new CapuccinoCoffee();
+        starbuzz.prepareCoffee();
 
-        starbuzz.prepareCapuccinoCoffee();
+        System.out.println("********************");
+        starbuzz = new AmericanoCoffee();
+        starbuzz.prepareCoffee();
 
     }
 
